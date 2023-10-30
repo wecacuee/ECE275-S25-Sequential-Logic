c725f5a9cd15abc8d2b54c28b47d4e0c32654f07
Vikas Dhiman <wecacuee@users.noreply.github.com>
2023-10-30 10:58:22 -0400

    next handout

---
+++ docs/slides/1130-FSM-optimization/generate-1030-full-handout.sh
@@ -0,0 +1,2 @@
+#pdftk ../1121-mealy-moore-seq-detector/solution\ to\ 1128\ example.pdf ../1121-mealy-moore-seq-detector/verilog/seqdetector_top.pdf ../../lab_pdfs/FAQs/SystemVerilog_FAQs.pdf 1130-FSM-optimization.pdf cat output 1130-full-handout.pdf 
+pdftk 1030-FSM-optimization.pdf ../1121-mealy-moore-seq-detector/solution\ to\ 1128\ example.pdf cat output 1030-full-handout.pdf
--- docs/slides/1130-FSM-optimization/generate-1130-full-handout.sh
+++
@@ -1,1 +0,0 @@
-pdftk ../1121-mealy-moore-seq-detector/solution\ to\ 1128\ example.pdf ../1121-mealy-moore-seq-detector/verilog/seqdetector_top.pdf ../../lab_pdfs/FAQs/SystemVerilog_FAQs.pdf 1130-FSM-optimization.pdf cat output 1130-full-handout.pdf 
